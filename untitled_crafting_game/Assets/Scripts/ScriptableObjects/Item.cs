8e06f00c820497f53f3484b5247fab6a339dfb68
cuecharles <cuecharles@gmail.com>
2020-08-12 17:32:45 -0700

    Test Scene - Tutorial on UI for Mobile and ScriptableObjects

---
+++ untitled_crafting_game/Assets/Scripts/ScriptableObjects/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Item", menuName = "ScriptableObjects/Item", order = 1)]
+public class Item : ScriptableObject
+{
+    public string name;
+    public string description;
+    public Sprite sprite;
+}
---
+++ untitled_crafting_game/Assets/Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    public Item itemToGet;
+    public Image inventoryImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TEST_BUTTON_PRESS()
+    {
+        print("HELLO!");
+        inventoryImage.sprite = itemToGet.sprite;
+    }
+
+}
